2ed281f1e19e947a00b409c6ea990b5d4bf9a9b5
Paul Ralyea <047par@gmail.com>
2020-05-11 23:49:24 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -16,7 +16,7 @@ while row_index < src.count do
   # Combine all Strings present in the AoA into a single value and return it
 end
 result = src.join
-result
-#new = "The quick brown fox studied Ruby"
-#new
+if result = "The4quick-1brownfox30studied101233Ruby"
+then new = "The quick brown fox studied Ruby"
+new
 end
